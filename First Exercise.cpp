cd799a529a75d1ded5f03ab9f08517c107031158
Tarek <Tarek@Ahmed>
2024-10-24 11:17:16 -0700

    this is first project in github.com

---
+++ First Exercise.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int ReadNumper(string Massage){
+    int Numper = 0;
+    do
+    {
+        cout << Massage;
+        cin >> Numper; //5
+    } while (Numper <= 0); // if Numper <= 0 try again
+    return Numper;
+}
+
+void pritnvalues(int Num){
+    for (int i = 1; i <= Num; i++)
+    {
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    pritnvalues(ReadNumper("Please Enter Your Number : "));
+}
